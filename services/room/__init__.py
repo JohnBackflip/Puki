3349f3bd1a05b7b9bb27ac5ab65a6b6f5e78a4e8
jz3218 <jiazhenglim.2023@scis.smu.edu.sg>
2025-03-26 12:07:52 +0800

    moved everything to coding

--- services/room/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes the room service directory a Python package 
--- services/room/middleware/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file makes the middleware directory a Python package 
-
-"""Room service middleware package""" 
